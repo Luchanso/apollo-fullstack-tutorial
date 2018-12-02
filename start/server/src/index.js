74b3d69fdc1b084b88c55ef8e52c3745b2f3956f
Oleg Loutchansky <luchansodev@gmail.com>
2018-12-02 23:37:55 +0300

    Add authorization

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require("apollo-server");
+const isEmail = require("isemail");
 const { createStore } = require("./utils");
 const typeDefs = require("./schema");
 const LaunchAPI = require("./datasources/launch");
@@ -13,7 +14,20 @@ const server = new ApolloServer({
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
-    })
+    }),
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = (req.headers && req.headers.authorization) || "";
+        const email = new Buffer(auth, "base64").toString("ascii");
+
+        // if the email isn't formatted validly, return null for user
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] ? users[0] : null;
+
+        return { user: { ...user.dataValues } };
+    }
 });
 
 server.listen().then(({ url }) => {
